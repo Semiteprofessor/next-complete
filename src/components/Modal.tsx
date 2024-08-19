4b7fd23d153d1f68da0ce972673da3ff90878be1
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-19 11:41:17 +0100

    Commit

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,40 @@
-import React from "react";
+"use client";
+
+import { useRouter } from "next/navigation";
+import { MouseEventHandler, useCallback, useEffect, useRef } from "react";
 
 const Modal = ({ children }: { children: React.ReactNode }) => {
+  const overlay = useRef(null);
+  const wrapper = useRef(null);
+  const router = useRouter();
+
+  const onDismiss = useCallback(() => {
+    router.back();
+  }, [router]);
+
+  const onClick: MouseEventHandler = useCallback(
+    (e) => {
+      if (e.target === overlay.current || e.target === wrapper.current) {
+        if (onDismiss) onDismiss();
+      }
+    },
+    [onDismiss, overlay, wrapper]
+  );
+
+  const onKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        if (onDismiss) onDismiss();
+      }
+    },
+    [onDismiss]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", onKeyDown);
+    return () => document.addEventListener("keydown", onKeyDown);
+  }, [onKeyDown]);
+
   return <div>{children}</div>;
 };
 
