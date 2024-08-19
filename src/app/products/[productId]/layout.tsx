d3d44cfbc16065fbd59419501318862be3108e28
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-19 06:43:05 +0100

    Commit

--- src/app/products/[productId]/layout.tsx
+++ src/app/products/[productId]/layout.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+const getRandomInt = (count: number) => {
+  return Math.floor(Math.random() * count);
+};
 
 const ProductDetailsLayout = ({ children }: { children: React.ReactNode }) => {
+  const random = getRandomInt(2);
+
+  if (random === 1) {
+    throw new Error("Error loading product details");
+  }
   return (
     <div>
       {children}
